85fd3a3a296043caaa8687efe0eed1afebc8fef0
Chelsea Spangler <56610933+CNSpangler@users.noreply.github.com>
2020-05-22 13:51:52 -0700

    refactor ternaries in seed files, extract logic to a function in utils.js (#14)
    * refactor seed-test-covid.js
    
    * refactor seed-covid.js
    
    * extract orNull function to utils.js
    
    * refactor seed-mobility.js and seed-test-mobility.js

--- db/seed-test-mobility.js
+++ db/seed-test-mobility.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
+const orNull = require('../lib/utils/utils');
 const TestMobilityData = require('../lib/models/TestMobilityData');
 const TestMobilityDataFile = require('../data/Test-Global_Mobility_Report-sample.json');
 
@@ -23,16 +24,16 @@ const seedData = () => {
   }) => (
     {
       date: date,
-      countryCode: country_region_code !== '' ? country_region_code : null,
-      countryName: country_region !== '' ? country_region : null,
-      subRegion1: sub_region_1 !== '' ? sub_region_1 : null,
-      subRegion2: sub_region_2 !== '' ? sub_region_2 : null,
-      retailChange: retail_and_recreation_percent_change_from_baseline !== '' ? retail_and_recreation_percent_change_from_baseline : null,
-      groceryChange: grocery_and_pharmacy_percent_change_from_baseline !== '' ? grocery_and_pharmacy_percent_change_from_baseline : null,
-      parksChange: parks_percent_change_from_baseline !== '' ? parks_percent_change_from_baseline : null, 
-      transitChange: transit_stations_percent_change_from_baseline !== '' ? transit_stations_percent_change_from_baseline : null,
-      workplacesChange: workplaces_percent_change_from_baseline !== '' ? workplaces_percent_change_from_baseline : null,
-      residentialChange: residential_percent_change_from_baseline !== '' ? residential_percent_change_from_baseline : null,
+      countryCode: orNull(country_region_code),
+      countryName: orNull(country_region),
+      subRegion1: orNull(sub_region_1),
+      subRegion2: orNull(sub_region_2),
+      retailChange: orNull(retail_and_recreation_percent_change_from_baseline),
+      groceryChange: orNull(grocery_and_pharmacy_percent_change_from_baseline),
+      parksChange: orNull(parks_percent_change_from_baseline), 
+      transitChange: orNull(transit_stations_percent_change_from_baseline),
+      workplacesChange: orNull(workplaces_percent_change_from_baseline),
+      residentialChange: orNull(residential_percent_change_from_baseline)
     }
   )));
 };
